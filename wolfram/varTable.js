3f046f81ecc624e98a83442f3df3767f911495e1
Mark Mba <markjet7@gmail.com>
2023-08-27 20:17:53 -0500

    weekend sync

--- wolfram/varTable.js
+++ wolfram/varTable.js
@@ -1,3 +1,1 @@
-{
-	"feedstock":"{<|\"stageID\" -> 2., \"areaCode\" -> 1001., \"name\" -> \"Construction & demolition waste\", \"facilityID\" -> 177453., <<1>>, \"railDistance\" -> 367.42826031, \"tons\" -> 4044.|>, <<57695>>, <|<<7>>|>}"
-}
+{}
